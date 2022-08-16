695ec5f3882c67d490496d4740b0a343ad1c4936
shpunk <ignas.vaitkus7@gmail.com>
2022-08-16 08:51:22 +0300

    Created a the component hierarchy in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import Wrapper from "./Components/Wrapper/Wrapper";
+import Screen from "./Components/Screen/Screen";
+import ButtonBox from "./Components/ButtonBox/ButtonBox";
+import Button from "./Components/Button/Button";
 
-function App() {
-  return (<>
-  </>);
-}
+const App = () => {
+  return (
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
+  );
+};
 
 export default App;
