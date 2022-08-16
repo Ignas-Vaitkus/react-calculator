6b352529696eec8467ff3296e69b96db40258bab
shpunk <ignas.vaitkus7@gmail.com>
2022-08-16 09:08:52 +0300

    Added signClickHandler

--- src/App.js
+++ src/App.js
@@ -47,6 +47,19 @@ const App = () => {
     });
   };
 
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
   return (
     <Wrapper>
       <Screen value="0" />
